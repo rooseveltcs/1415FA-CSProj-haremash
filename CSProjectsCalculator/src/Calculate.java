8e91d6d41394e810378bc55ef3de84cf24ccf002
typeHaller <halle@remash.com>
2014-09-22 09:44:58 -0700

    Added all number buttons. Not finished with clear functions. Must format add opperators and opperator functionallity.

--- CSProjectsCalculator/src/Calculate.java
+++ CSProjectsCalculator/src/Calculate.java
@@ -9,29 +9,79 @@ import javax.swing.JTextField;
 public class Calculate extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
+
+	public JTextField nonEditableText = new JTextField(10);
 	public JTextField text = new JTextField(10);
+
+	public JButton button0 = new JButton("0");
+	public JButton button1 = new JButton("1");
+	public JButton button2 = new JButton("2");
+	public JButton button3 = new JButton("3");
+	public JButton button4 = new JButton("4");
+	public JButton button5 = new JButton("5");
+	public JButton button6 = new JButton("6");
+	public JButton button7 = new JButton("7");
+	public JButton button8 = new JButton("8");
+	public JButton button9 = new JButton("9");
+	public JButton buttonClear = new JButton("CLEAR");
 
 	public Calculate() {
 
 		this.getContentPane().setLayout(new FlowLayout());
-		JButton button1 = new JButton("Yes");
-		JButton button2 = new JButton("No");
 
+		nonEditableText.setEditable(false);
+
+		text.addActionListener(this);
+		text.addActionListener(this);
+		button0.addActionListener(this);
 		button1.addActionListener(this);
 		button2.addActionListener(this);
-		text.addActionListener(this);
+		button3.addActionListener(this);
+		button4.addActionListener(this);
+		button5.addActionListener(this);
+		button6.addActionListener(this);
+		button7.addActionListener(this);
+		button8.addActionListener(this);
+		button9.addActionListener(this);
+		buttonClear.addActionListener(this);
+		add(nonEditableText);
+		add(text);
+		add(button0);
 		add(button1);
 		add(button2);
-		add(text);
+		add(button3);
+		add(button4);
+		add(button5);
+		add(button6);
+		add(button7);
+		add(button8);
+		add(button9);
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent ae) {
 		String action = ae.getActionCommand();
-		if (action.equals("Yes")) {
-			System.out.println("Yes Button pressed!");
-		} else if (action.equals("No")) {
-			System.out.println(text.getText());
+		if (action.equals("0")) {
+			nonEditableText.setText("0");
+		} else if (action.equals("1")) {
+			nonEditableText.setText("1");
+		} else if (action.equals("2")) {
+			nonEditableText.setText("2");
+		} else if (action.equals("3")) {
+			nonEditableText.setText("3");
+		} else if (action.equals("4")) {
+			nonEditableText.setText("4");
+		} else if (action.equals("5")) {
+			nonEditableText.setText("5");
+		} else if (action.equals("6")) {
+			nonEditableText.setText("6");
+		} else if (action.equals("7")) {
+			nonEditableText.setText("7");
+		} else if (action.equals("8")) {
+			nonEditableText.setText("8");
+		} else if (action.equals("9")) {
+			nonEditableText.setText("9");
 		}
 	}
 
